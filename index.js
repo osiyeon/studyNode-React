88b1808beba99b4e7ba7a32ce76b3f104a6e8316
osiyeon <osiyun0313@gmail.com>
2020-07-24 15:51:56 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://osiyeon:980313dhtldus@cluster0.5tihp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('config.mongoURI', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log(`MongoDB Connected...`))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어줌. -> user 모델 가져와야 함. 
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app ilstening on port ${port}!`))
 
