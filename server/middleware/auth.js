7cd139bc7b4b70f406e90e880970d4acf38e6c91
osiyeon <osiyun0313@gmail.com>
2020-07-31 15:56:48 +0900

    20.07.31까지

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+const { User } = require('../models/User')
+
+let auth = (req, res, next) => {
+
+    // 인증 처리를 하는 곳
+
+    // 클라이언트 쿠키에서 토큰을 가져 옴.
+    let token = req.cookies.x_auth
+
+    // 토큰을 복호화 한 후  유저를 찾음
+    User.findByToken(token, (err, user) => {
+        if(err) throw err
+
+        // 유저가 없으면 인증 No
+        if(!user) return res.json({ isAuth: false, error: true })
+
+        // 유저가 있으면 인정 Okay
+        req.token = token
+        req.user = user
+        next()
+    })
+}
+
+
+module.exports = { auth }
